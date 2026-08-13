33a586df2c6bbb1253a548d52e7f5c3f24327e60
macbook017 <sagar.bhalshing@bond.ai>
2026-08-13 15:37:56 +0530

    Updated Code

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
+  basePath: "/portfolio-website",
+  assetPrefix: "/portfolio-website/",
+  trailingSlash: true,
   typescript: {
     ignoreBuildErrors: true,
   },
