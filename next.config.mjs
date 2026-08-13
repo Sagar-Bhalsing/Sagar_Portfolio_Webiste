1545947eeb8794b7f9cfb6aa6f15f0ca3fa01a3e
macbook017 <sagar.bhalshing@bond.ai>
2026-08-13 15:48:20 +0530

    Update GitHub Pages base path

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  basePath: "/portfolio-website",
-  assetPrefix: "/portfolio-website/",
+  basePath: "/Sagar_Portfolio_Webiste",
+  assetPrefix: "/Sagar_Portfolio_Webiste/",
   trailingSlash: true,
   typescript: {
     ignoreBuildErrors: true,
